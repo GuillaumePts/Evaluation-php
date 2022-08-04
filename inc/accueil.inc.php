9416b6490e5e27c64fcf8e827cbaaa74dcf3f99a
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-08-05 00:20:16 +0200

    j'ai fais en sort que dans l'accueil soit affiché les 4 derniers articles rédigés

--- inc/accueil.inc.php
+++ inc/accueil.inc.php
@@ -1,1 +1,23 @@
 <h2>Acceuil</h2>
+
+<h3>Articles à la une</h3>
+<?php 
+
+$sql = "SELECT * FROM articles WHERE status = 'visible' ORDER BY created_at DESC ";
+global $pdo;
+$query = $pdo->prepare($sql);
+$query->execute();
+$articlesacc = $query->fetchAll();
+
+dump($articlesacc[0]);
+dump($articlesacc[1]);
+dump($articlesacc[2]);
+dump($articlesacc[3]);
+// foreach($articlesacc as $value){
+    
+// }
+
+
+// for ($i=0; $i < 4 ; $i++) { 
+//    dump($value);
+// }
