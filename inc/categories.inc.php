ebdceb861ce277f7078914c247c12690e457fa97
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-08-04 23:26:46 +0200

    j'ai réglé 2 problèmes; on peut plus chercher une categorie inexistante et lorsqu'il n'y a pas d'article dans une categorie un message l'indique bien entendu tout est très moche je rend juste le site fonctionnel j'attaquerais le style à la fin

--- inc/categories.inc.php
+++ inc/categories.inc.php
@@ -1,27 +1,41 @@
-<h2>categorie = <?php  echo $_GET['categorie'];?></h2>
+
 
 <?php 
+$lacategorie = $_GET['categorie'];
+$sqlverif = "SELECT * FROM categories WHERE nom ='$lacategorie' ";
 
+global $pdo;
+ $query = $pdo->prepare($sqlverif);
+ $query->execute();
+ $verif = $query->fetchAll();
 
+ if(empty($verif)){
+    echo '<p>Cette categorie n\'éxiste pas </p>';
+ }else{
 
-if(!empty($_GET['categorie'])){
-    $lacategorie= $_GET['categorie'];
+echo '<h1>'.$lacategorie.'</h1>';
+echo '<h2>Articles</h2>';
 
     $sql= "SELECT * FROM articles Where categorie = '$lacategorie'";
    
-    global $pdo;
-$query = $pdo->prepare($sql);
-$query->execute();
-$articles = $query->fetchAll();
+         global $pdo;
+     $query = $pdo->prepare($sql);
+     $query->execute();
+     $articles = $query->fetchAll();
 
-?>
+     if(empty($articles)){
+        echo '<p>Aucuns articles dans cette categorie </p> ';
+     }else{
+        foreach($articles as $article){
+            dump($article);
+        }
+     }
 
-<p>Articles</p>
+      
+ }
 
-<?php
 
-foreach($articles as $article){
-    dump($article);
-}
 
-}
+
+
+
