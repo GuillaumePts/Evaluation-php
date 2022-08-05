b906c8f63e9e3408dc325b5997e40bcc5bd8e9fe
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-08-05 23:32:29 +0200

    voila par défault la personne qui s'inscrit à le role utilisateur; inscription fini je vais maintenant passer à la connexion qui va etre plus compliqué vis à vis de la gestion des roles

--- inc/function.php
+++ inc/function.php
@@ -31,20 +31,21 @@ function verifierUtilisateur($email) {
     }
 }
 
-function inscrireUtilisateur(string $nom, string $prenom, string $email, string $mdp): bool {
+function inscrireUtilisateur(string $nom, string $prenom, string $email, string $mdp, string $roles='utilisateur'): bool {
     $mdp = password_hash("$mdp", PASSWORD_DEFAULT);
     ;
 
     global $pdo;
     if ($pdo) {
         $requeteInscription = "INSERT INTO users
-        (nom, prenom, email, mdp)
-        VALUES (:nom, :prenom, :email, :mdp)";
+        (nom, prenom, email, mdp, roles)
+        VALUES (:nom, :prenom, :email, :mdp, :roles)";
         $query = $pdo->prepare($requeteInscription);
         $query->bindValue(':nom', $nom, PDO::PARAM_STR);
         $query->bindValue(':prenom', $prenom, PDO::PARAM_STR);
         $query->bindValue(':email', $email, PDO::PARAM_STR);
         $query->bindValue(':mdp', $mdp, PDO::PARAM_STR);
+        $query->bindValue(':roles', $roles, PDO::PARAM_STR);
         $query->execute();
         return true;
     } else {
